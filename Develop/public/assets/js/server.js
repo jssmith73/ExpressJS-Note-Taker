e5458f98129c68c5ae72ab7a99cdde466ccf1b18
jman1359 <josiahsmith1359@gmail.com>
2023-10-07 18:53:59 -0400

    added server.js

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3002;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.send('Navigate to /index or /notes'));
+
+app.get('/index', (req, res) => 
+   res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req,res) =>
+   res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.listen(PORT, () => 
+console.log(`Listening at http://localhost:${PORT}`)
+);
